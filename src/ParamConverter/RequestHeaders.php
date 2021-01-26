0d964de833d065f40ee2ca5bf0fe0444de57b86d
Eduard Sukharev <sukharev.eh@gmail.com>
2021-01-26 13:59:24 +0300

    Improved name argument conversion

--- src/ParamConverter/RequestHeaders.php
+++ src/ParamConverter/RequestHeaders.php
@@ -13,7 +13,8 @@ final class RequestHeaders implements ParamConverterInterface
      */
     public function apply(Request $request, ParamConverter $configuration)
     {
-        $headerName = trim(preg_replace('/([A-Z])/', '-$1', $configuration->getName()), '-');
+        $parameterName = lcfirst(str_replace('-', '', $configuration->getName()));
+        $headerName = trim(preg_replace('/([A-Z])/', '-$1', $parameterName), '-');
 
         if (!$request->headers->has($headerName) && !$configuration->isOptional()) {
             throw new MissingHeaderException($headerName);
@@ -29,7 +30,7 @@ final class RequestHeaders implements ParamConverterInterface
             $object = (bool)$object;
         }
 
-        $request->attributes->set($configuration->getName(), $object);
+        $request->attributes->set($parameterName, $object);
 
         return true;
     }
